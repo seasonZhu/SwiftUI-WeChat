be18eae36c10948000ab6ede28bb10a17fedb151
zhujilong <zhujilong2007@sina.com>
2020-02-08 14:38:59 +0800

    SwiftUI自定义返回很困难

--- SwiftUI-WeChat/Module/Profile/ProfileView.swift
+++ SwiftUI-WeChat/Module/Profile/ProfileView.swift
@@ -9,13 +9,35 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var isPresented: Bool = false
+    
     var body: some View {
         Text("待开发")
+            .navigationBarTitle("哈哈")
+            .navigationBarItems(leading:
+                Button(action: {
+                    print("点击了返回按钮")
+                    // 如果不用系统的 我根本就不知道该怎么返回好不好!!!
+                }) {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text("back")
+                    }
+                }
+            )
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+struct CustomBar: View {
+    var body: some View {
+        Text("Hello world")
+            .foregroundColor(.secondary)
+            .font(.system(.largeTitle))
     }
 }
