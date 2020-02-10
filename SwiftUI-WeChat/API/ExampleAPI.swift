3295a248656042bf5bb99db097c057ac0a2d82ed
zhujilong <zhujilong2007@sina.com>
2020-02-10 11:05:21 +0800

    ExampleAPI 对于如何在SwiftUI中进行网络请求,我感觉传统的方式好像不怎么友好

---
+++ SwiftUI-WeChat/API/ExampleAPI.swift
@@ -0,0 +1,110 @@
+//
+//  ExampleAPI.swift
+//  SwiftUI-WeChat
+//
+//  Created by dy on 2020/2/10.
+//  Copyright © 2020 Gesen. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+//MARK:- API
+let ExampleProvider = MoyaProvider<ExampleAPI>(plugins: [NetworkLoggerPlugin(verbose: true)])
+
+enum ExampleAPI {
+    case topic
+}
+
+extension ExampleAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://sun.topray-media.cn")!
+    }
+    
+    var path: String {
+        switch self {
+        case .topic:
+            return "/tz_inf/api/topics"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .topic:
+            return .post
+        }
+    }
+    
+    var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+    
+    var task: Task {
+        var parmeters:[String:Any] = [:]
+        switch self {
+        case .topic:
+            parmeters = [:]
+        }
+        return .requestParameters(parameters: parmeters, encoding: URLEncoding.default)
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+}
+
+//MARK:- API对应的模型
+struct ExampleModel: Codable {
+    var list: [ListItem]?
+    var code: Int?
+}
+
+struct ListItem: Codable {
+    var topicOrder: Int?
+    var id: Int?
+    var topicDesc: String?
+    var topicTittle: String?
+    var upTime: String?
+    var topicImageUrl: String?
+    var topicStatus: Int?
+}
+
+
+class Topic: ObservableObject, Codable {
+    var list: [TopicItem]?
+    var code: Int?
+}
+
+class TopicItem: ObservableObject, Codable {
+    var topicOrder: Int?
+    var id: Int?
+    var topicDesc: String?
+    var topicTittle: String?
+    var upTime: String?
+    var topicImageUrl: String?
+    var topicStatus: Int?
+}
+
+//MARK:- 封装请求结果与其回调
+enum ResponseResult<T: Codable, E: Error> {
+    case success(T?)
+    case error(E)
+}
+
+typealias ResponseResultCallback<T: Codable, E: Error> = (ResponseResult<T, E>) -> Void
+
+//MARK:- ViewModel
+class ViewModel {
+    
+    static func getTopics(callback: @escaping ResponseResultCallback<[TopicItem], MoyaError>) {
+        ExampleProvider.request(.topic) { (result) in
+            switch result {
+            case .success(let response):
+                let list = try? response.map(Topic.self).list
+                callback(.success(list))
+            case .failure(let error):
+                callback(.error(error))
+            }
+        }
+    }
+}
--- SwiftUI-WeChat/Module/Chat/ChatView.swift
+++ SwiftUI-WeChat/Module/Chat/ChatView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct ChatView: View {
     @State var text: String = ""
     
+    @State var list: [TopicItem]?
+    
+    @State var isNetWorkSuccess = false
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Text("待开发")
@@ -18,10 +22,50 @@ struct ChatView: View {
             TextView(text: $text)
                 .foregroundColor(.black)
                 .font(.system(size: 28))
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 44)
                 .padding()
-        }.navigationBarTitle("聊天", displayMode: .inline)
+            
+            if list != nil {
+                List {
+                    ForEach(list!, id: \.id) {
+                        Text($0.topicTittle ?? "")
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationBarTitle("聊天", displayMode: .inline)
+        .onAppear {
+            //self.exampleApiRequest()
+            self.viewModelRequest()
+        }
         
+    }
+    
+    func exampleApiRequest() {
+        ExampleProvider.request(.topic) { (result) in
+            switch result {
+            case .success(let response):
+                //let model = try? response.map(ExampleModel.self)
+                //let listItems = try? response.map([ListItem].self, atKeyPath: ".list")
+                self.list = try? response.map(Topic.self).list
+                self.isNetWorkSuccess = true
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func viewModelRequest() {
+        ViewModel.getTopics { (result) in
+            switch result {
+            case .success(let list):
+                self.list = list
+                self.isNetWorkSuccess = true
+            case .error(let error):
+                print(error)
+            }
+        }
     }
 }
 
